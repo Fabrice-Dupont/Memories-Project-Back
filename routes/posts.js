4eadbc6d4a9da0147a238b56704bd985143de9c3
Fabrice <dupont.fabrice62@gmail.com>
2022-09-03 11:55:28 +0200

    create a post (1:53)

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
-import express from 'express';
+import express from 'express'
 
-import { getPosts, createPost } from '../controllers/posts.js';
+import { createPost, getPosts, updatePost } from '../controllers/posts.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', getPosts);
-router.post('/', createPost);
+router.get('/', getPosts)
+router.post('/', createPost)
+router.patch('/:id', updatePost)
 
-export default router;
+export default router
