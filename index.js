3b87ed2e986814c4568b605294debcfc303f506b
Fabrice <dupont.fabrice62@gmail.com>
2022-09-28 23:12:35 +0200

    adding Auth component (2:48)

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
 
-import postRoutes from './routes/posts.js';
+import postRoutes from './routes/posts.js'
 //const express = require('express')
 
-const app = express();
+const app = express()
+dotenv.config()
 
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
+app.use('/posts', postRoutes)
+//const CONNECTION_URL = 'mongodb+srv://FabriceDupont:AndrewMongoLewis62@cluster0.q2depnp.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5001
 
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch(error => console.log(error.message))
 
-app.use('/posts', postRoutes);
-const CONNECTION_URL = 'mongodb+srv://FabriceDupont:AndrewMongoLewis62@cluster0.q2depnp.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5001;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-.catch((error)=> console.log(error.message));
-
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false)
 
 // https://www.mongodb.com/cloud/atlas
