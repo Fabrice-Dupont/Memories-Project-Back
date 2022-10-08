9c102657ee6704a02aecc2e3845a3eba916b0379
Fabrice <dupont.fabrice62@gmail.com>
2022-10-08 21:42:21 +0200

    authentification completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 //const express = require('express')
 
 const app = express()
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
 //const CONNECTION_URL = 'mongodb+srv://FabriceDupont:AndrewMongoLewis62@cluster0.q2depnp.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5001
 
